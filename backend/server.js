d02b65059314b66da923f54883d2b181785911ee
Rohan Singh <riskysingh897@gmail.com>
2024-09-24 10:08:47 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from 'express'
+import authRoutes from './routes/auth.route.js'
 
 const app = express()
 
-app.get('/', (req,res)=>{
-    res.send('Server is ready!!')
-})
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(5000, ()=>{
     console.log('Server started at port 5000')
