6b3b3f68f15039924cb3499a7fc4d930e4c7c67f
Rohan Singh <riskysingh897@gmail.com>
2024-10-03 00:14:08 +0530

    create details, similar and catagory movies api

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -26,4 +26,41 @@ export async function getMoviesTrailers(req, res) {
     }
 }
 
+export async function getMoviesDetails(req, res) {
+    const { id } = req.params
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`)
+        res.status(200).json({success: true, content: data})
+    } catch (error) {
+        if(error.message.includes('404')){
+            return res.status(404).send(null)
+        }
+
+        res.status(500).json({success: false, message: 'Internal Server Error'})
+    }
+}
+
+export async function getSimilarMovies(req, res) {
+    const { id } = req.params
+    try {
+        const data = fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`)
+        res.status(200).json({success: true, similar: data.results})
+    } catch (error) {
+        res.status(500).json({success:false, message:'Internal Server Error'})
+    }
+}
+
+
+export async function getMoviesByCatagory(req, res) {
+    const { catagory } = req.params
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${catagory}?language=en-US&page=1`)
+        res.status(200).json({success: true, content: data.results})
+    } catch (error) {
+        res.status(500).json({success:false, message:'Internal Server Error'})
+    }
+}
+
+
+
 
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getTrendingMovie, getMoviesTrailers } from '../controllers/movie.controller.js'
+import { getTrendingMovie, getMoviesTrailers, getMoviesDetails, getSimilarMovies, getMoviesByCatagory } from '../controllers/movie.controller.js'
 
 const router = express.Router()
 
 router.get('/trending', getTrendingMovie)
 router.get('/:id/trailers', getMoviesTrailers)
+router.get('/:id/details', getMoviesDetails)
+router.get('/:id/similar', getSimilarMovies)
+router.get('/:catagory', getMoviesByCatagory)
 
 export default router
