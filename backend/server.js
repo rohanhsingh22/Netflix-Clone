8bfd14c3686e1c1768f919ac65ae338519b26184
Rohan Singh <riskysingh897@gmail.com>
2024-10-03 12:06:09 +0530

    create the search apis

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.route.js'
 import movieRoutes from './routes/movie.route.js'
 import tvRoutes from './routes/tv.route.js'
+import searchRoutes from './routes/search.route.js'
 import { protectRoute } from './middleware/protectRoute.js'
 
 import { ENV_VARS } from './config/envVars.js'
@@ -21,6 +22,7 @@ app.use(cookieParser())
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/movie', protectRoute, movieRoutes)
 app.use('/api/v1/tv', protectRoute, tvRoutes)
+app.use('/api/v1/search', protectRoute, searchRoutes)
 
 app.listen(5000, ()=>{
     console.log('Server started at port ' + PORT)
