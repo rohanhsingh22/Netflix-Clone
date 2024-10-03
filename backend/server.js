b016ee4ecbaf5045433eed4d45dabd171bb74a0c
Rohan Singh <riskysingh897@gmail.com>
2024-10-03 10:50:23 +0530

    create middleware to protect routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.route.js'
 import movieRoutes from './routes/movie.route.js'
 import tvRoutes from './routes/tv.route.js'
+import { protectRoute } from './middleware/protectRoute.js'
 
 import { ENV_VARS } from './config/envVars.js'
 import { connectDB } from './config/db.js'
+
 
 const app = express()
 
@@ -13,10 +16,11 @@ const app = express()
 const PORT = ENV_VARS.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/movie', movieRoutes)
-app.use('/api/v1/tv', tvRoutes)
+app.use('/api/v1/movie', protectRoute, movieRoutes)
+app.use('/api/v1/tv', protectRoute, tvRoutes)
 
 app.listen(5000, ()=>{
     console.log('Server started at port ' + PORT)
